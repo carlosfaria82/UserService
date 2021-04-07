61798c3618b3cebe8d398c8278577e34484fa9ea
carlos.faria <carlos.faria@onmobile.com>
2021-04-07 12:12:09 +0200

    Adding initial migration

--- UserService.Api/Controllers/UserController.cs
+++ UserService.Api/Controllers/UserController.cs
@@ -9,7 +9,7 @@ using UserService.Application.Services;
 namespace UserService.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class UserController 
         : ControllerBase
     {
